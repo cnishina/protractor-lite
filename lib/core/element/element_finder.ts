d993e6d8d4233211588b5a86a1ea8e9149405601
Craig Nishina <craig.nishina@gmail.com>
2019-02-11 02:32:50 -0800

    chore(element): getWebElement, isPresent, and count
    - Set element finder properties to private.

--- lib/core/element/element_finder.ts
+++ lib/core/element/element_finder.ts
@@ -23,8 +23,8 @@ const ACTION_OPTIONS: ActionOptions = {
 
 export class ElementFinder {
 
-  constructor(public browser: Browser, public locator: Locator,
-    public getWebElements: GetWebElements) {
+  constructor(private _browser: Browser, private _locator: Locator,
+    private _getWebElements: GetWebElements) {
   }
 
   /**
@@ -35,10 +35,10 @@ export class ElementFinder {
   async clear(actionOptions: ActionOptions = ACTION_OPTIONS
       ): Promise<ElementFinder> {
     const action = async (): Promise<void> => {
-      const webElements = await this.getWebElements();
-      await webElements[0].clear();
+      const webElement = await this.getWebElement();
+      await webElement.clear();
     };
-    await runAction(action, actionOptions, this.browser);
+    await runAction(action, actionOptions, this._browser);
     return this;
   }
 
@@ -51,11 +51,24 @@ export class ElementFinder {
       ): Promise<ElementFinder> {
     // Gets the first web element matching the locator and clicks on it.
     const action = async (): Promise<void> => {
-      const webElements = await this.getWebElements();
-      await webElements[0].click();
+      const webElement = await this.getWebElement();
+      await webElement.click();
     };
-    await runAction(action, actionOptions, this.browser);
+    await runAction(action, actionOptions, this._browser);
     return this;
+  }
+
+  /**
+   * The count of web elements.
+   * @return A promise of the number of elements matching the locator.
+   */
+  async count(): Promise<number> {
+    try {
+      const webElements = await this._getWebElements();
+      return webElements.length;
+    } catch (err) {
+      throw err;
+    }
   }
 
   /**
@@ -67,10 +80,10 @@ export class ElementFinder {
   getAttribute(attributeName: string,
       actionOptions: ActionOptions = ACTION_OPTIONS): Promise<string> {
     const action = async (): Promise<string> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].getAttribute(attributeName);
+      const webElement = await this.getWebElement();
+      return webElement.getAttribute(attributeName);
     };
-    return runAction(action, actionOptions, this.browser);
+    return runAction(action, actionOptions, this._browser);
   }
 
   /**
@@ -80,10 +93,20 @@ export class ElementFinder {
    */
   getTagName(actionOptions: ActionOptions = ACTION_OPTIONS): Promise<string> {
     const action = async (): Promise<string> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].getTagName();
+      const webElement = await this.getWebElement();
+      return webElement.getTagName();
     };
-    return runAction(action, actionOptions, this.browser);
+    return runAction(action, actionOptions, this._browser);
+  }
+
+  /**
+   * Returns the WebElement represented by this ElementFinder. This method does
+   * not retry. Throws the WebDriver error if the element doesn't exist.
+   * @return A promise of the WebElement.
+   */
+  async getWebElement(): Promise<WebElement> {
+    const webElements = await this._getWebElements();
+    return webElements[0];
   }
 
   /**
@@ -94,10 +117,10 @@ export class ElementFinder {
   getText(actionOptions: ActionOptions = ACTION_OPTIONS
       ): Promise<string> {
     const action = async (): Promise<string> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].getText();
+      const webElement = await this.getWebElement();
+      return webElement.getText();
     };
-    return runAction(action, actionOptions, this.browser);
+    return runAction(action, actionOptions, this._browser);
   }
 
   /**
@@ -108,10 +131,10 @@ export class ElementFinder {
   isDisplayed(actionOptions: ActionOptions = ACTION_OPTIONS
       ): Promise<boolean> {
     const action = async (): Promise<boolean> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].isDisplayed();
+      const webElement = await this.getWebElement();
+      return webElement.isDisplayed();
     };
-    return runAction(action, actionOptions, this.browser);
+    return runAction(action, actionOptions, this._browser);
   }
 
   /**
@@ -122,10 +145,22 @@ export class ElementFinder {
   isEnabled(actionOptions: ActionOptions = ACTION_OPTIONS
       ): Promise<boolean> {
     const action = async (): Promise<boolean> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].isEnabled();
+      const webElement = await this.getWebElement();
+      return webElement.isEnabled();
     };
-    return runAction(action, actionOptions, this.browser);
+    return runAction(action, actionOptions, this._browser);
+  }
+
+  /**
+   * Checks if the web element is present.
+   * @param actionOptions Optional options for retries and functionHooks.
+   */
+  isPresent(actionOptions: ActionOptions = ACTION_OPTIONS
+      ): Promise<boolean> {
+    const action = async (): Promise<boolean> => {
+      return await this.count() >= 1;
+    };
+    return runAction(action, actionOptions, this._browser);
   }
 
   /**
@@ -136,10 +171,10 @@ export class ElementFinder {
   isSelected(actionOptions: ActionOptions = ACTION_OPTIONS
       ): Promise<boolean> {
     const action = async (): Promise<boolean> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].isSelected();
+      const webElement = await this.getWebElement();
+      return webElement.isSelected();
     };
-    return runAction(action, actionOptions, this.browser);
+    return runAction(action, actionOptions, this._browser);
   }
 
   /**
@@ -150,10 +185,10 @@ export class ElementFinder {
   async sendKeys(keys: string|number,
       actionOptions: ActionOptions = ACTION_OPTIONS): Promise<ElementFinder> {
     const action = async (): Promise<void> => {
-      const webElements = await this.getWebElements();
-      return webElements[0].sendKeys(keys);
+      const webElement = await this.getWebElement();
+      return webElement.sendKeys(keys);
     };
-    await runAction(action, actionOptions, this.browser);
+    await runAction(action, actionOptions, this._browser);
     return this;
   }
 }

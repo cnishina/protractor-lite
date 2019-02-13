7272f706562091934db54250616f8cd5b6b705f2
Craig Nishina <craig.nishina@gmail.com>
2019-02-13 14:25:30 -0800

    chore(element.all): set properties to private
    - Set ElementArrayFinder to use WebDriver or WebElement instead of
    browser.
    - Change locator from method to getter property.
    - Export browser config in index.

--- lib/core/index.ts
+++ lib/core/index.ts
@@ -2,6 +2,8 @@ import { Browser, BrowserConfig } from './browser';
 import { By } from './by';
 import { buildElementHelper } from './element';
 
+export { BrowserConfig } from './browser';
+
 /**
  * Builds the objects for protractor that use the same selenium browser session.
  * @param config A configuration object with a capabilities property.

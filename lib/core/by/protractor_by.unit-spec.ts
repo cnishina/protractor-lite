378b77a5d0678ecaa37272cb749b4851f4a4ec7e
Craig Nishina <cnishina@users.noreply.github.com>
2019-02-27 02:09:22 -0800

    chore(finders): add more finders (#55)
    - cssContainingText
    - deepCss
    - partialButtonText

--- lib/core/by/protractor_by.unit-spec.ts
+++ lib/core/by/protractor_by.unit-spec.ts
@@ -9,8 +9,33 @@ describe('protractor_by', () => {
 
   describe('by button text', () => {
     it('should create a protractor locator', () => {
-      let locator = protractorBy.buttonText('foobar');
+      let locator = protractorBy.buttonText('search text');
       expect(typeof locator.findElementsOverride).toBe('function');
+      expect(typeof locator.toString).toBe('function');
+    });
+  });
+
+  describe('by css containing text', () => {
+    it('should create a protractor locator', () => {
+      let locator = protractorBy.cssContainingText('css', 'search text');
+      expect(typeof locator.findElementsOverride).toBe('function');
+      expect(typeof locator.toString).toBe('function');
+    });
+  });
+
+  describe('by deep css', () => {
+    it('should create a protractor locator', () => {
+      let locator = protractorBy.deepCss('css');
+      expect(typeof locator.findElementsOverride).toBe('function');
+      expect(typeof locator.toString).toBe('function');
+    });
+  });
+
+  describe('by deep css', () => {
+    it('should create a protractor locator', () => {
+      let locator = protractorBy.partialButtonText('search text');
+      expect(typeof locator.findElementsOverride).toBe('function');
+      expect(typeof locator.toString).toBe('function');
     });
   });
 });

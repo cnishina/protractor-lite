557827d837bfd3b3ace9d86cf5a38c05f6743363
Craig Nishina <craig.nishina@gmail.com>
2019-02-26 23:59:44 -0800

    chore(browser): add methods to browser
    - addCookie, deleteCookie, getAllCookies, getCookie
    - getAllWindowHandles, getWindowHandle
    - navigateBack, navigateForward, navigateTo
    - switchToFrame, switchToParent, switchToWindow
    - close, getCapabilities, getPageSource, refresh, sleep, , takeScreenshot, wait

---
+++ lib/core/utils/rectangle.ts
@@ -0,0 +1,9 @@
+/**
+ * Interface for WebElement rectangles to find the position and dimensions.
+ */
+export interface Rectangle {
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+}

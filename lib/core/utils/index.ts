befb181cd9fdda74b4ff40e0456c3e907fbbb51b
Craig Nishina <cnishina@users.noreply.github.com>
2019-03-11 17:37:46 -0700

    chore(task): add task helpers (#60)

--- lib/core/utils/index.ts
+++ lib/core/utils/index.ts
@@ -1,6 +1,7 @@
+export * from './task_helpers';
 export { Capabilities } from './capabilities';
 export { Cookie } from './cookie';
 export { Rectangle } from './rectangle';
-export { SharedResults } from './shared_results';
+export { sharedResultsInit, SharedResults } from './shared_results';
 export { runAction } from './task';
-export { TaskOptions } from './task_options';
+export { taskOptionsInit, TaskOptions } from './task_options';
--- lib/core/utils/task_options.ts
+++ lib/core/utils/task_options.ts
@@ -1,4 +1,42 @@
 /**
+ * Since these options is just an interface, initialize objects and create empty
+ * arrays.
+ * @param taskOptions The task options.
+ * @return The initialized task options.
+ */
+export function taskOptionsInit(taskOptions: TaskOptions): TaskOptions {
+  if (!taskOptions) {
+    taskOptions = {
+      retries: 1,
+      useDefaults: true,
+      validate: true,
+    };
+  }
+  if (!taskOptions.tasks) {
+    taskOptions.tasks = {};
+  }
+  if (!taskOptions.tasks.before) {
+    taskOptions.tasks.before = {};
+  }
+  if (!taskOptions.tasks.before.browser) {
+    taskOptions.tasks.before.browser = [];
+  }
+  if (!taskOptions.tasks.before.local) {
+    taskOptions.tasks.before.local = [];
+  }
+  if (!taskOptions.tasks.after) {
+    taskOptions.tasks.after = {};
+  }
+  if (!taskOptions.tasks.after.browser) {
+    taskOptions.tasks.after.browser = [];
+  }
+  if (!taskOptions.tasks.after.local) {
+    taskOptions.tasks.after.local = [];
+  }
+  return taskOptions;
+}
+
+/**
  * Function hooks for browser and local execution.
  */
 export interface Task {
@@ -33,4 +71,6 @@ export interface TaskEvents {
 export interface TaskOptions {
   tasks?: TaskEvents;
   retries?: number;
+  useDefaults?: boolean;
+  validate?: boolean;
 }
